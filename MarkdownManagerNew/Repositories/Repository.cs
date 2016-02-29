4754ec812892520e1f0f974c4b6876303c643e2d
aaalex91 <alexander.edin@hotmail.com>
2016-02-29 14:23:29 +0100

    Repo

---
+++ MarkdownManagerNew/Repositories/Repository.cs
@@ -0,0 +1,31 @@
+﻿using MarkdownManagerNew.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MarkdownManagerNew.Repositories
+{
+    public class Repository
+    {
+        private ApplicationDbContext dbContext;
+        private RoleStore<IdentityRole> roleStore;
+        private RoleManager<IdentityRole> roleManager;
+        private UserStore<ApplicationUser> userStore;
+        private UserManager<ApplicationUser> userManager;
+
+        public Repository()
+        {
+            this.dbContext = new ApplicationDbContext();
+            this.roleStore = new RoleStore<IdentityRole>(dbContext);
+            this.roleManager = new RoleManager<IdentityRole>(roleStore);
+            this.userStore = new UserStore<ApplicationUser>(dbContext);
+            this.userManager = new UserManager<ApplicationUser>(userStore);  
+        }
+
+
+    }
+}
