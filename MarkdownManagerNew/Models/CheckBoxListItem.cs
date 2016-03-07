bbaf8cadcf22b9180973b5fba49be25859065d9b
philiped2 <philip.edin.alt@gmail.com>
2016-03-07 13:43:04 +0100

    Createdocument almost finished and changed navlinks to swedish

---
+++ MarkdownManagerNew/Models/CheckBoxListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarkdownManagerNew.Models
+{
+    public class CheckBoxListItem
+    {
+        public string ID { get; set; }
+        public string Display { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
